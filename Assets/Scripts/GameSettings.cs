df95d4fc3dacea09de9e81bd95c7a906379a56a8
Steve Kane <kanesteven@gmail.com>
2018-07-15 18:58:29 -0500

    Added GameSettings and ability to control background music

---
+++ Assets/Scripts/GameSettings.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameSettings : MonoBehaviour 
+{
+	[Header("Audio")]
+	public bool PlayBackgroundMusic = true;
+	[Range(0f, 1f)]
+	public float BackgroundMusicVolume = .6f;
+}
