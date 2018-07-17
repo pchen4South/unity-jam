d74ebff5a80dbb21b6068f9d8765ea67a3e7c50d
Peter Chen <pchen@4-south.com>
2018-07-17 16:14:20 -0500

    hadouken w charging logic

--- Assets/Scripts/Hadouken.cs
+++ Assets/Scripts/Hadouken.cs
@@ -17,4 +17,17 @@ public class Hadouken : MonoBehaviour {
     [Header("State")]
     public int PlayerNumber;
 	
+	void OnCollisionEnter(Collision other)
+    {
+        //all projectile colliding game objects should be tagged "Enemy" or whatever in inspector but that tag must be reflected in the below if conditional
+        if (other.gameObject.tag == "Player" && PlayerNumber != other.gameObject.GetComponent<Player>().PlayerNumber)
+        {
+			var target = other.gameObject.GetComponent<Player>();
+			target.Health -= 1;
+        }
+		if(other.gameObject.tag != "Ground" || 
+		(other.gameObject.tag == "Player" && PlayerNumber != other.gameObject.GetComponent<Player>().PlayerNumber)){
+			Destroy(gameObject);
+		}
+    }
 }
--- Assets/Scripts/HadoukenLauncher.cs
+++ Assets/Scripts/HadoukenLauncher.cs
@@ -6,10 +6,10 @@ public class HadoukenLauncher : AbstractWeapon {
 
     [Header("Cached references")]
     [SerializeField]
-    AudioSource fireSound;
+    AudioSource[] fireSound;
 
     [Header("Prefabs")]
-    public Hadouken Ammo;
+    public Hadouken[] Ammo;
 
     [Header("Config")]
     public float TravelSpeed = 25f;
@@ -18,20 +18,69 @@ public class HadoukenLauncher : AbstractWeapon {
     private float nextFire = 0f;
     public float fireRate = 0.5f;
 
+    private float chargeTime = 0f;
+
+
     public override void PullTrigger(Player player)
     {
         if (Time.time < nextFire)
             return;
         
-        var wep = player.Weapon;
-        var nade = Instantiate(Ammo, wep.transform.position + wep.transform.forward * 1.025f, wep.transform.rotation);
-        nade.body.AddForce(nade.transform.forward * TravelSpeed, ForceMode.Impulse);
 
-        /*
-        nade.PlayerNumber = player.PlayerNumber;
-        fireSound.Play();
-         */
-        nextFire = Time.time + fireRate;
+        chargeTime += Time.deltaTime;
+
     
     }
+
+    public override void ReleaseTrigger(Player player){
+
+        var ChargeLevel = 0;
+        var wep = player.Weapon;
+        
+        if (chargeTime >= 1f && chargeTime <2f)
+            ChargeLevel = 1;
+        else if (chargeTime >= 2f){
+            ChargeLevel = 2;
+        }
+        fireSound[ChargeLevel].Play();
+
+        if(ChargeLevel == 2)
+            StartCoroutine(DelayFire(0.6f, ChargeLevel, wep, player));
+        else
+        {
+         ShootFireball(ChargeLevel, wep, player);
+        }
+        
+        nextFire = Time.time + fireRate;
+        chargeTime = 0f;
+    }
+
+    IEnumerator DelayFire(float delayTime, int ChargeLevel, AbstractWeapon wep, Player player){
+            yield return new WaitForSeconds(delayTime);
+            ShootFireball(ChargeLevel, wep, player);
+    }
+    void ShootFireball(int ChargeLevel, AbstractWeapon wep, Player player){
+            float forceMultiplier = 0;
+
+            switch (ChargeLevel){
+                case 0:
+                    forceMultiplier = 1;
+                    break;
+                case 1:
+                    forceMultiplier = 1.5f;
+                    break;
+                case 2:
+                    forceMultiplier = 2;
+                    break;
+                default:
+                    forceMultiplier = 1;
+                    break;
+            }
+
+
+            var fireball = Instantiate(Ammo[ChargeLevel], wep.transform.position + wep.transform.forward * 1.02f , wep.transform.rotation);
+            fireball.body.AddForce(fireball.transform.forward * TravelSpeed * forceMultiplier, ForceMode.Impulse);
+            fireball.PlayerNumber = player.PlayerNumber;
+    }
+
 }
