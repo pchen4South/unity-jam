77a57d239d7cfb22c3be0b62bfd01d11287fe5f4
Peter Chen <pchen@4-south.com>
2018-07-16 23:20:01 -0500

    included hadouken files

---
+++ Assets/Scripts/HadoukenLauncher.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HadoukenLauncher : AbstractWeapon {
+
+    [Header("Cached references")]
+    [SerializeField]
+    AudioSource fireSound;
+
+    [Header("Prefabs")]
+    public Hadouken Ammo;
+
+    [Header("Config")]
+    public float GrenadeTravelSpeed = 8f;
+
+    [Header("State")]
+    private float nextFire = 0f;
+    public float fireRate = 0.5f;
+
+    public override void PullTrigger(Player player)
+    {
+        if (Time.time < nextFire)
+            return;
+        
+        var wep = player.Weapon;
+        var nade = Instantiate(Ammo, wep.transform.position, wep.transform.rotation);
+
+        nade.body.AddForce(nade.transform.forward * GrenadeTravelSpeed, ForceMode.Impulse);
+        nade.PlayerNumber = player.PlayerNumber;
+        fireSound.Play();
+        nextFire = Time.time + fireRate;
+    }
+}
