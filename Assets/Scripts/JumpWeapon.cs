3cb68d39f923b08466161f99d7cec4e2d01e1c19
Peter Chen <pchen@4-south.com>
2018-07-18 16:02:20 -0500

    finished up overheating feature of machinegun

--- Assets/Scripts/JumpWeapon.cs
+++ Assets/Scripts/JumpWeapon.cs
@@ -9,9 +9,19 @@ public class JumpWeapon : AbstractWeapon
 	[SerializeField]
 	AnimationCurve beamDirectionCurve;
 
+	[Header("Prefabs")]
+	public WeaponBar ChargeBar;
 	[Header("Properties")]
 	public float MaxRange = 100f;
 	public float FiringDuration = 1f;
+
+	//weapon bar stuff
+	public Color BarColor1 = Color.white;
+    public Color BarColor2 = Color.yellow;
+    public Color BarColor3 = Color.red;
+	private WeaponBar BarInstance;
+	// Added maxchargetime for max time you can float in the air
+	public float FullChargeTime = 1f;
 
 	[Header("Cached Variables")]
 	Ray ray = new Ray();
@@ -21,12 +31,21 @@ public class JumpWeapon : AbstractWeapon
 	WeaponState state = WeaponState.Ready;
 	float remainingFiringTime = 0f;
 	float firingPower = 0f;
+	float chargeTime = 0f;
 
+
+	private void Start(){
+		var bar = Instantiate(ChargeBar, player.transform.position + player.transform.up * 1.02f , player.transform.rotation, player.transform);
+		bar.player = player;
+		bar.maxBarColor = BarColor3;
+		BarInstance = bar;
+	}
 	public override void PullTrigger(Player p)
 	{
 		if (state == WeaponState.Ready && !p.isGrounded)
 		{
 			state = WeaponState.Charging;
+			chargeTime = 0;
 		}
 	}
 
@@ -37,6 +56,7 @@ public class JumpWeapon : AbstractWeapon
 			state = WeaponState.Firing;
 			remainingFiringTime = FiringDuration;
 			firingPower = p.aerialHeight;
+			BarInstance.slider.value = 0;
 		}
 	}
 
@@ -54,6 +74,26 @@ public class JumpWeapon : AbstractWeapon
 			case WeaponState.Charging:
 				player.rooted = true;
 				lineRenderer.enabled = false;
+
+			   // Weapon bar stuff
+				chargeTime += Time.deltaTime;
+				var img = BarInstance.img;
+        		var percentCharged = (chargeTime <= FullChargeTime ? chargeTime : FullChargeTime) / FullChargeTime;
+				 BarInstance.slider.value = percentCharged;
+
+				if (percentCharged < 0.5)
+				{
+					img.color = Color.Lerp(BarColor1, BarColor2, (float)percentCharged / 0.5f);
+				}
+				else if (percentCharged > 0.5 && percentCharged < 1)
+				{
+					img.color = Color.Lerp(BarColor2, BarColor3, (float)((percentCharged - 0.5f)/ 0.5f));
+				} 
+				else if (percentCharged == 1 && chargeTime == FullChargeTime)
+				{
+					img.color = BarColor3;
+				}
+
 				break;
 
 			// if firing, update/render the beam and check for meaningful collisions
