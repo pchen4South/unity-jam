c388c90f9cd7eb66ec5e37bc1d8c8c83c070d29f
Peter Chen <pchen@4-south.com>
2018-07-17 20:30:10 -0500

    added weapon bar prefab and attached to hadouken launcher

---
+++ Assets/Scripts/WeaponBar.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+public class WeaponBar : MonoBehaviour {
+	
+	[Header("Prefabs")]
+	public Player player;
+    Camera m_Camera;
+	
+
+    void Start() {
+        m_Camera = Camera.main;
+        var slider = gameObject.GetComponentInChildren<Slider>();
+        slider.value = 0;
+    }
+    
+    void Update()
+    {
+        transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
+                         m_Camera.transform.rotation * Vector3.up);
+
+		transform.position = player.transform.position + Vector3.up * 2f;
+        var slider = gameObject.GetComponentInChildren<Slider>();
+    }
+}
