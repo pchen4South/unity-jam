f1befe2a998c40a9213c8607b80ca50f5379c47d
Peter Chen <pchen@4-south.com>
2018-07-18 22:36:16 -0500

    added spray nade

---
+++ Assets/Scripts/SprayNade.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class SprayNade : MonoBehaviour 
+{
+    [Header("Cached components")]
+    public Rigidbody body;
+
+    [Header("Configuration")]
+    public float fuseTime = 1f;
+    private float timeAlive = 0f;
+
+    [Header("Prefabs")]
+    public GrenadeExplosion Explosion;
+    
+    [Header("State")]
+    public int PlayerNumber;
+
+    void Start(){
+        Debug.Log("Spraying Nades");
+    }
+
+
+	void Update() 
+    {
+        //grow larger over time test
+        //transform.localScale += new Vector3(0.5f,0.5f,0.5f) * Time.deltaTime;
+        timeAlive += Time.deltaTime;
+
+        if (timeAlive >= fuseTime) 
+        {
+            var explosion = Instantiate(Explosion, gameObject.transform.position, gameObject.transform.rotation);
+
+            explosion.PlayerNumber = PlayerNumber;
+            Destroy(gameObject);
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (!(other.gameObject.tag == "Player" && other.gameObject.GetComponent<Player>().PlayerNumber == PlayerNumber))
+        {
+            var explosion = Instantiate(Explosion, gameObject.transform.position, gameObject.transform.rotation);
+            explosion.PlayerNumber = PlayerNumber;
+            Destroy(gameObject);
+        } 
+    }
+}
