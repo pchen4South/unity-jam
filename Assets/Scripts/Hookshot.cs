dc6ba8a3daad04eaecadcd1d173d4df27e666067
Steve Kane <kanesteven@gmail.com>
2018-07-19 22:27:50 -0500

    added basic skeleton of hookshot. no functionality yet

---
+++ Assets/Scripts/Hookshot.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Hookshot : AbstractWeapon
+{
+	[SerializeField]
+	Hook hook;
+
+	public override void PullTrigger(Player player)
+	{
+		player.canMove = true;
+		player.canRotate = false;
+	}
+
+	public override void ReleaseTrigger(Player player)
+	{
+		player.canMove = false;
+		player.canRotate = true;
+	}
+
+	public void Update()
+	{
+
+	}
+}
