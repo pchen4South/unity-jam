9c1eef7ecec5cc6a4792f7a7d9f3dad22d55f506
Steve Kane <kanesteven@gmail.com>
2018-07-22 22:46:34 -0500

    made fade duration on chainAudio a property

--- Assets/Scripts/Hookshot.cs
+++ Assets/Scripts/Hookshot.cs
@@ -14,6 +14,7 @@ public class Hookshot : AbstractWeapon
 	AudioSource hookAudio;
 
 	public float MoveSpeed = 100f;
+	public float chainFadeDuration = .4f;
 	public float chainVolume = .6f;
 	public float hookVolume = .7f;
 
@@ -119,7 +120,7 @@ public class Hookshot : AbstractWeapon
 		player.canMove = true;
 		player.canRotate = true;
 		state = WeaponState.Ready;
-		chainFadeOut = chainAudio.FadeOutOver(.4f);
+		chainFadeOut = chainAudio.FadeOutOver(chainFadeDuration);
 		StartCoroutine(chainFadeOut);
 		Destroy(hook.gameObject);
 	}
