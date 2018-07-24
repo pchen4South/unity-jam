eca1861f3578451a7ef9b8a1edecd44147c32f9b
Steve Kane <kanesteven@gmail.com>
2018-07-23 19:55:08 -0500

    removed unneeded variables from JumpWeapon and formatting

--- Assets/Scripts/JumpWeapon.cs
+++ Assets/Scripts/JumpWeapon.cs
@@ -9,19 +9,9 @@ public class JumpWeapon : AbstractWeapon
 	[SerializeField]
 	AnimationCurve beamDirectionCurve;
 
-	[Header("Prefabs")]
-	public WeaponBar ChargeBar;
 	[Header("Properties")]
 	public float MaxRange = 100f;
 	public float FiringDuration = 1f;
-
-	//weapon bar stuff
-	public Color BarColor1 = Color.white;
-    public Color BarColor2 = Color.yellow;
-    public Color BarColor3 = Color.red;
-	private WeaponBar BarInstance;
-	// Added maxchargetime for max time you can float in the air
-	public float FullChargeTime = 1f;
 
 	[Header("Cached Variables")]
 	Ray ray = new Ray();
@@ -31,18 +21,12 @@ public class JumpWeapon : AbstractWeapon
 	WeaponState state = WeaponState.Ready;
 	float remainingFiringTime = 0f;
 	float firingPower = 0f;
-	float chargeTime = 0f;
 
-
-	private void Start(){
-
-	}
 	public override void PullTrigger(Player p)
 	{
 		if (state == WeaponState.Ready && !p.isGrounded)
 		{
 			state = WeaponState.Charging;
-			chargeTime = 0;
 		}
 	}
 
@@ -53,7 +37,6 @@ public class JumpWeapon : AbstractWeapon
 			state = WeaponState.Firing;
 			remainingFiringTime = FiringDuration;
 			firingPower = p.aerialHeight;
-			BarInstance.slider.value = 0;
 		}
 	}
 
@@ -100,13 +83,15 @@ public class JumpWeapon : AbstractWeapon
 					remainingFiringTime -= Time.deltaTime;
 
 					// deal damage to players
-					if (didHit && rayHit.collider.gameObject.tag == "Player")
+					if (didHit && rayHit.collider.gameObject.CompareTag("Player"))
 					{
 						var target = rayHit.collider.gameObject.GetComponent<Player>();	
 
 						// don't damage ourselves!
 						if (target != player)
+						{
 							target.Health -= 1;
+						}
 					}
 				}
 				else 
