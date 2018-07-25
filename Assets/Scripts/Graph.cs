0185e93387cf446025b7045a43e163520f1abccb
Steve Kane <kanesteven@gmail.com>
2018-07-24 21:34:49 -0500

    Added Graph prefab/scripts

---
+++ Assets/Scripts/Graph.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour 
+{
+	[SerializeField]
+	GraphBar[] graphBars;
+
+	public void UpdateBar(int barIndex, Color color, float normalizedScale)
+	{
+		graphBars[barIndex].backgroundImage.color = color;
+		graphBars[barIndex].transform.localScale = new Vector3(normalizedScale, 1, 1);
+	}
+}
---
+++ Assets/Scripts/GraphBar.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GraphBar : MonoBehaviour 
+{
+	public RawImage backgroundImage;
+	public RectTransform rectTransform;
+}
