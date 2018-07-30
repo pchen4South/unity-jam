6db637d4771ff69926fa7775f3e6af9c5aba934c
Steve Kane <kanesteven@gmail.com>
2018-07-29 19:33:16 -0500

    added PlayerIndicator prefab, material and shader

---
+++ Assets/Effects/PlayerIndicator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class PlayerIndicator : MonoBehaviour 
+{
+	public MeshRenderer meshRenderer;
+	public float rotationSpeed = 100f;
+
+	void Update()
+	{
+		var radians = rotationSpeed * Time.time;
+
+		meshRenderer.material.SetFloat("_SinAngle", Mathf.Sin(radians));
+		meshRenderer.material.SetFloat("_CosAngle", Mathf.Cos(radians));
+	}
+}
