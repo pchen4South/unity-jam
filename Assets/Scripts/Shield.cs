39be530b4b2202cc6c2aa4616e4c288d8b40042b
Peter Chen <pchen@4-south.com>
2018-09-19 23:49:19 -0500

    shield stuff

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Shield : AbstractWeapon
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    enum RiotShieldState {Ready, Swinging, Charging}
+    RiotShieldState shieldState = RiotShieldState.Ready;
+
+    [Header("Cached References")]
+    [SerializeField]
+    Animator anim;
+    [SerializeField]
+    public float swingTime = 1f;
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public override void PullTrigger(Player player){
+        if(shieldState != RiotShieldState.Ready)
+            return;
+        shieldState = RiotShieldState.Swinging;
+        anim.SetBool("swinging", true);
+    }
+
+    public override void ReleaseTrigger(Player player){
+        shieldState = RiotShieldState.Ready;
+        anim.SetBool("swinging", false);
     }
 }
