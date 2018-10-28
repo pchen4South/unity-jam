53af8832dfa5bba436e14031dd3066802c632507
Steve Kane <kanesteven@gmail.com>
2018-10-27 21:43:38 -0500

    formatting MapSelect script

--- Assets/Scripts/MapSelect_LoadOnClick.cs
+++ Assets/Scripts/MapSelect_LoadOnClick.cs
@@ -14,29 +14,37 @@ public class MapSelect_LoadOnClick : MonoBehaviour
     [SerializeField] Button OverlayButton;
     public Image CurrentMapImage;
     public Text CurrentMapLabel;
-
     AbstractMap currentMap;
     int mapIndex = 0;
     
-    public void LoadScene(string MapName){
+    public void LoadScene(string MapName)
+    {
         LoadingScreen.SetActive(true);
         SceneManager.LoadScene(MapName);
     }
-    void Start(){
+
+    void Start()
+    {
         ChangeMap();
     }
-    void Update(){
-        if(mapIndex == 0){
+
+    void Update()
+    {
+        if(mapIndex == 0)
+        {
             UpArrow.interactable = false;
             DownArrow.interactable = true;
-        } else if (mapIndex == Maps.Length - 1){
+        } 
+        else if (mapIndex == Maps.Length - 1)
+        {
             UpArrow.interactable = true;
             DownArrow.interactable = false;
-        } else {
+        } 
+        else 
+        {
             UpArrow.interactable = true;
             DownArrow.interactable = true;
         }
-
 
         //poll players controllers 
 		var players = ReInput.players;
@@ -44,35 +52,37 @@ public class MapSelect_LoadOnClick : MonoBehaviour
 			var player = players.GetPlayer(i);
 			if(player.controllers.joystickCount > 0){
 				var startPressed = player.GetButtonDown("Left");
-				if(startPressed){
+				if(startPressed)
+                {
 				
 				}
 			}
 		}
-		
     }
 
-    public void UpArrowClicked(){
+    public void UpArrowClicked()
+    {
         if (mapIndex == 0) return;
         mapIndex --;
         ChangeMap();
     }
 
-    public void DownArrowClicked(){
+    public void DownArrowClicked()
+    {
         if (mapIndex == Maps.Length -1) return;
         mapIndex++;
         ChangeMap();
     }
 
-    void ChangeMap(){
+    void ChangeMap()
+    {
         currentMap = Maps[mapIndex];
         CurrentMapImage.sprite  = currentMap.MapScreenshot.sprite;
         CurrentMapLabel.text = currentMap.MapName.text;
     }
 
-    public void MapClick(){
+    public void MapClick()
+    {
         LoadScene(CurrentMapLabel.text);
     }
-
-
 }
