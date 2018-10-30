c9465fea13db8f899c69de75ba8f618b0ec67e31
Peter Chen <pchen@4-south.com>
2018-10-29 20:54:18 -0500

    added boss example to miniboss game

--- Assets/Scripts/Minigame.cs
+++ Assets/Scripts/Minigame.cs
@@ -20,12 +20,15 @@ public abstract class Minigame : MonoBehaviour
     enum MG_State { Ready, Running, Ended, ResultsReady}
     [SerializeField] public float MinigameDuration = 5f;
     [SerializeField] public float MinigameAliveTimer = 0f;
+    
     MG_State MinigameState = MG_State.Ready;
+
 
     public MinigameResults Results = new MinigameResults();
 
     public void BeginMinigame(){
         if(MinigameState == MG_State.Ready){
+            PrepareMinigame();
             MinigameState = MG_State.Running;
             MinigameAliveTimer = 0f;
         }
@@ -48,13 +51,23 @@ public abstract class Minigame : MonoBehaviour
                 break;
         }
     }
-    public virtual void HandleMove(PlayerState p) {}
-    public virtual void HandleDash(PlayerState p) {}
-    public virtual void HandleRotate(PlayerState p) {}
-    public virtual void HandleFire(PlayerState p) {}
+    public virtual void HandleMove(PlayerState p) {
+        InputHelpers.BasicMove(p);
+    }
+    public virtual void HandleDash(PlayerState p) {
+        InputHelpers.BasicDash(p);
+    }
+    public virtual void HandleRotate(PlayerState p) {
+        InputHelpers.BasicRotate(p);
+    }
+    public virtual void HandleFire(PlayerState p) {
+        InputHelpers.BasicPullTrigger(p);
+        InputHelpers.BasicReleaseTrigger(p);
+    }
     public virtual void HandlePlayerDamage(PlayerState attacker, PlayerState victim) {}
     public virtual void HandleMinigameCompleted(){}
     public virtual void TabulateResults(){}
+    public virtual void PrepareMinigame() {}
 
     public void SetMinigameToReady(){ MinigameState = MG_State.Ready; }
     public void SetMinigameToResultsReady(){ MinigameState = MG_State.ResultsReady; }
--- Assets/Scripts/Minigames/mg_bossbattle.cs
+++ Assets/Scripts/Minigames/mg_bossbattle.cs
@@ -2,29 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+// Parent methods
+// public virtual void HandleMove(PlayerState p) {}
+// public virtual void HandleDash(PlayerState p) {}
+// public virtual void HandleRotate(PlayerState p) {}
+// public virtual void HandleFire(PlayerState p) {}
+// public virtual void HandlePlayerDamage(PlayerState attacker, PlayerState victim) {}
+
 public class mg_bossbattle : Minigame
 {
 
-    // Parent methods
-    // public virtual void HandleMove(PlayerState p) {}
-    // public virtual void HandleDash(PlayerState p) {}
-    // public virtual void HandleRotate(PlayerState p) {}
-    // public virtual void HandleFire(PlayerState p) {}
-    // public virtual void HandlePlayerDamage(PlayerState attacker, PlayerState victim) {}
-    void Start()
-    {
-        Debug.Log("boss battle has started");
+    [SerializeField] public GameObject Boss;
+    [SerializeField] public GameObject[] StageElementsToModify;
+
+    public override void PrepareMinigame(){
+        var boss_instance = Instantiate(Boss);
+        StageElementsToModify = GameObject.FindGameObjectsWithTag("Disable_BossBattle");
+        foreach(var ele in StageElementsToModify){
+            ele.SetActive(false);
+        }
+        Destroy(boss_instance, MinigameDuration);
     }
 
     public override void HandleMinigameCompleted(){
         Debug.Log("boss battle ended");
+        foreach(var ele in StageElementsToModify){
+            ele.SetActive(true);
+        }
     }
     public override void HandleMove(PlayerState p) {
         InputHelpers.BasicMove(p);
     }
 
     public override void TabulateResults(){
-        Debug.Log("results are compiled and ready");
         SetMinigameToResultsReady();
     }
 
